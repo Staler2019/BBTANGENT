4246eead3ac777d740b7c8d0209cacde273b6628
Staler2019 <abcd121234@yahoo.com.tw>
2020-05-16 00:28:04 +0800

    v1.0.0 initilaization of MAIN: game OTHERS: ball, block; create README.md and the assets folder

---
+++ README.md
@@ -0,0 +1,9 @@
+# 計算機概論 期末專題
+## BBTANGENT
+## Content
+參考 111% 公司出品的「BBTAN」，決定重製一個不包含功能球的致敬作品。前期只有打算製作方格下降的類型。
+---
+# Author
+name: 楊佳峻
+student ID: 108502571
+class: B
---
+++ src/game/game.java
@@ -0,0 +1,32 @@
+package game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class game extends Application implements Initializable {
+    // writing fxml setting
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(".fxml")); // TODO: Making .fxml
+        primaryStage.setTitle("BBTANGENT Game");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+    }
+
+    // end writing fxml setting
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
